3490da8a32d63130ff981c3aa0e5c08e1a2d2b1a
cassiabaptiston <cassiabaptiston@gmail.com>
2022-05-28 12:03:24 -0300

    javascrip

---
+++ primeiro.js
@@ -0,0 +1,18 @@
+function div(n1, n2) {
+    return n1/n2;
+}
+
+const a="Cassia"
+function name(a){
+    return a="Cassia";
+}
+
+function name2(a,b){
+    return a+b;
+}
+
+   const a = "Cassia", b = "André",c = "Felipe", d = "Nilce", e = "Moisés", f = "Erick";
+undefined
+function alunos() {
+    return [a,b,c,d,e,f];
+}
