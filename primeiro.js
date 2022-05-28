03795464934f53385177079e93261c15e50daaaa
cassiabaptiston <cassiabaptiston@gmail.com>
2022-05-28 12:58:11 -0300

    Exercício Completo

--- primeiro.js
+++ primeiro.js
@@ -1,18 +1,38 @@
+//exercicio 3
 function div(n1, n2) {
     return n1/n2;
 }
 
+//exercicio 1
 const a="Cassia"
 function name(a){
     return a="Cassia";
 }
 
+exercicio 2
 function name2(a,b){
     return a+b;
 }
 
+//exercicio 4
    const a = "Cassia", b = "André",c = "Felipe", d = "Nilce", e = "Moisés", f = "Erick";
 undefined
 function alunos() {
     return [a,b,c,d,e,f];
 }
+
+//exercicio 5
+var arr = ["Cassia"]
+undefined
+function exer(a,b){
+    a = arr
+    b = "Baptiston"
+    return a+b;
+}
+undefined
+exer()
+'CassiaBaptiston'
+arr.push("Chagas")
+2
+exer()
+'Cassia,ChagasBaptiston'
