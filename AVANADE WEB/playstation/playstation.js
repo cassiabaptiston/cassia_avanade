8830ef23afd4ff7b68394b90c51aa6c0daefe962
cassiabaptiston <cassiabaptiston@gmail.com>
2022-06-01 15:15:51 -0300

    teste

---
+++ AVANADE WEB/playstation/playstation.js
@@ -0,0 +1,6 @@
+function login() {
+    var email = document.getElementById('caixa1').value;
+    var senha = document.getElementById('caixa2').value;
+
+    alert(`Usuário ${email} e Senha ${senha} incorretos`);
+}
