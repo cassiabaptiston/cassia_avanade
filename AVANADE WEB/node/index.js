7a0d7b01bc259fef97941e614e2e000b838a6a44
cassiabaptiston <cassiabaptiston@gmail.com>
2022-06-02 16:18:54 -0300

    Exercício 01/06/2022 - Par ou Impar

--- AVANADE WEB/node/index.js
+++ AVANADE WEB/node/index.js
@@ -2,13 +2,32 @@ function teste () {
     return {nome: "Marcos",
     escrevernome: function (){
     console.log(this.nome);
- },
- mae: {nome: "Iza", idade: 62}, 
- idade: 25,
- habilitado: false,
- filhos: [{nome: "José", idade: 50}, {nome: "Maria", idade: 0}],
- cursos: ["JS", "JAVA", "C#"], 
- corCabelo: "Preto", 
- altura: 1.75};
+    },
+    mae: {nome: "Iza", idade: 62}, 
+    idade: 25,
+    habilitado: false,
+    filhos: [{nome: "José", idade: 50}, {nome: "Maria", idade: 0}],
+    cursos: ["JS", "JAVA", "C#"], 
+    corCabelo: "Preto", 
+    altura: 1.75
+    };
 }
-console.log(teste);
+console.log(teste());
+
+function filhoscommaisdeumano () {
+    return teste().filhos.filter((filhos) => {return filhos.idade > 1});
+}
+console.log(filhoscommaisdeumano());
+
+/////////
+
+function parouimpar (n1){
+    var n1 = 7
+    console.log(`O número ${n1} é par?`)
+    if (n1%2 == 0){
+        return true
+    }else{
+        return false
+    };
+}
+console.log(parouimpar());
